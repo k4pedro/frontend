3d12b16328843b481a1202a5653c021303f646e9
Pedro Lima <145206042+k4pedro@users.roreply.github.com>
2025-10-17 12:14:27 -0300

    first commit front-end

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+export function App() {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <div className='max-w-3xl px-6'> 
+      <p className='text-lg text-gray-700'>Bem-vindo ao nosso site!</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
